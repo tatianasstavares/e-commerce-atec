f10a5b80021aff03c8cbc7797a96df6660e39c46
Tatiana Tavares <62269978+tatianasstavares@users.noreply.github.com>
2022-06-19 19:55:01 +0100

    created init method to load and fill store with products

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+import { getAllProductsFromApi } from './presenter/products.js'
+import { getStoreFromLocalStorage, manipulateStore, saveInitialProductsToStore, store } from './store/store.js';
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+async function init() {
+  try {
+    const storeFromLocalStorage = getStoreFromLocalStorage()
+    if (Boolean(storeFromLocalStorage)) {
+      manipulateStore(storeFromLocalStorage)
+    } else {
+      const productsFromApi = await getAllProductsFromApi();
+      saveInitialProductsToStore(productsFromApi)
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
 
+init()
 
