4ebff7f0efdae3d04322d26a11c252946f261233
Tatiana Tavares <62269978+tatianasstavares@users.noreply.github.com>
2022-06-25 19:19:26 +0100

    feat:add components scripts, header and footer on view .

---
+++ view/components/header/header.js
@@ -0,0 +1,17 @@
+const header = `
+    <div class="welcome-message">
+        <p>Bem-vindo a Botique Kirei</p>
+    </div>
+    <nav>
+        <p><a href="./shop.html">Boutique Kirei</a></p>
+        <ul class="container-icons">
+            <li><a href="./cart.html"><img src="../assets/icons/shopping-bag-icon.png" alt="shopping bag"></a></li>
+            <li><a href="./about.html"><img src="../assets/icons/about-icon.png" alt="about page"></a></li>
+            <li><a href="./contact.html"><img src="../assets/icons/whatsapp-icon.png" alt="whatsapp icon"></a></li>
+        </ul>
+    </nav>
+`
+
+export {
+    header
+}
