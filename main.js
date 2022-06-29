d7a55a566bd0ec2d00ab39f4c24441b192e3e11b
Tatiana Tavares <62269978+tatianasstavares@users.noreply.github.com>
2022-06-29 11:39:48 +0100

    feat:made adjustments to responsiveness

--- main.js
+++ main.js
@@ -2,11 +2,8 @@ import { getAllProductsFromApi } from './presenter/products.js'
 import { getStoreFromLocalStorage, manipulateStore, saveInitialProductsToStore, store } from './store/store.js';
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
-async function init() {
+
+export async function init() {
   try {
     const storeFromLocalStorage = getStoreFromLocalStorage()
     if (Boolean(storeFromLocalStorage)) {
