3f0ec33ffb21caa1224fffddcf3db389ad4fb117
Tatiana Tavares <62269978+tatianasstavares@users.noreply.github.com>
2022-06-30 14:58:46 +0100

    feat:create mainProductCard component

--- presenter/shop/shop.js
+++ presenter/shop/shop.js
@@ -1,6 +1,13 @@
+import { store } from "../../store/store.js"
 import { loadHeaderAndfooter } from "../../view/components/loadComponents.js"
-
+import { renderMainProductCard } from "../../view/components/shop/mainProductCard.js"
 loadHeaderAndfooter()
 
+const productsContainerEl = document.querySelector(".products-container")
 
+const products = store.products.map(product => renderMainProductCard(product)).join("")
 
+// const products = renderMainProductCard(store.products[0])
+productsContainerEl.innerHTML = products
+
+console.log(products, store);
