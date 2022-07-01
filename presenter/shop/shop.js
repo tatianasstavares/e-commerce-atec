8e1fdaf3144e90de428890247767c62341c8d0e1
Tatiana Tavares <62269978+tatianasstavares@users.noreply.github.com>
2022-07-01 10:35:45 +0100

    add search products

--- presenter/shop/shop.js
+++ presenter/shop/shop.js
@@ -1,13 +1,31 @@
 import { store } from "../../store/store.js"
 import { loadHeaderAndfooter } from "../../view/components/loadComponents.js"
 import { renderMainProductCard } from "../../view/components/shop/mainProductCard.js"
+
 loadHeaderAndfooter()
 
 const productsContainerEl = document.querySelector(".products-container")
+const searchInputEl = document.querySelector('.search-input')
 
-const products = store.products.map(product => renderMainProductCard(product)).join("")
+function loadShopScreen(products) {
+    console.log(store);
+    const productsList = products.map(product => renderMainProductCard(product)).join("")
+    productsContainerEl.innerHTML = productsList
+}
 
-// const products = renderMainProductCard(store.products[0])
-productsContainerEl.innerHTML = products
+function searchProducts() {
+    const searchTerm = searchInputEl.value.toLowerCase();
+    const filterAddProducts = store.products.filter((product) => {
+        const index = product.title.toLowerCase().indexOf(searchTerm)
 
-console.log(products, store);
+        return index >= 0
+    })
+
+    loadShopScreen(filterAddProducts)
+}
+
+searchInputEl.addEventListener('input', searchProducts)
+
+loadShopScreen(store.products)
+
+
