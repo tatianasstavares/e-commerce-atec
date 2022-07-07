21be64c0328369dbb74dfc434876d36ba98cb16b
Tatiana Tavares <62269978+tatianasstavares@users.noreply.github.com>
2022-07-07 12:22:52 +0100

    fix: position of contact pop up

--- presenter/contact/contact.js
+++ presenter/contact/contact.js
@@ -3,19 +3,14 @@ import { init } from "../../main.js"
 import { User } from "./addUser.js"
 import { sendMessage } from "./sendMessage.js"
 import { closePopup } from "./closePopup.js"
-/*function contact() {
-   loadHeaderAndfooter();
-   alert("2");
-   console.log("3");
-}
 
 await init()
+
 sendMessage()
 closePopup()
-if(window.localStorage.getItem('name1')==null||window.localStorage.getItem('name2')==null){
 
+if (window.localStorage.getItem('name1') == null || window.localStorage.getItem('name2') == null) {
    GetApiRandomUser()
 } else {
    User(2)
-   
-}*/
+}
